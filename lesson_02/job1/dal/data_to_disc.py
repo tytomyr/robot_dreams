80da6874632a2f68b34aac2ca2caba72a23cdf94
tytomyr <tytomyr.a@ajax.systems>
2024-10-03 16:31:14 +0300

    fix(data_to_disc): separate create_dir and create_file logic

--- lesson_02/job1/dal/data_to_disc.py
+++ lesson_02/job1/dal/data_to_disc.py
@@ -4,15 +4,21 @@ import shutil
 from pathlib import Path
 from lesson_02.job1.bll.get_data_from_api import GetDataFromAPI
 
-def create_file(raw_dir: str, data: GetDataFromAPI = None) -> None:
-    path = Path(__file__).parent.parent / raw_dir
-    if os.path.exists(path):
-        shutil.rmtree(path)
-    os.makedirs(path/"sales", exist_ok=True)
+
+def create_dir(raw_dir: str, path: Path) -> None:
+    dir_path = Path(__file__).parent.parent / raw_dir
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+    os.makedirs(path, exist_ok=True)
+
+
+def create_file(path: Path, data: GetDataFromAPI) -> None:
     for number, file  in enumerate(data.purchase_list):
-        with open(path/"sales"/f"sales_{file["purchase_date"]}_{number + 1}.json", "w") as f:
+        with open(path/f"sales_{file["purchase_date"]}_{number + 1}.json", "w") as f:
             json.dump(file, f)
 
 
 def main(raw_dir : str, data: GetDataFromAPI):
-    create_file(raw_dir, data)
+    path = Path(__file__).parent.parent / raw_dir / "sales" / data.purchase_list[0]["purchase_date"]
+    create_dir(raw_dir=raw_dir, path=path)
+    create_file(path=path, data=data)
