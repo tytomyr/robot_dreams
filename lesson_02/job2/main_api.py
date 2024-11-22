3a749efb89ff76b854c80b6283a2f9841f1b0727
tytomyr <tytomyr.a@ajax.systems>
2024-11-22 11:31:08 +0200

    feat: add convert_files to main api

--- lesson_02/job2/main_api.py
+++ lesson_02/job2/main_api.py
@@ -1,10 +1,16 @@
+import os
+
 from flask import Flask, request
 from pathlib import Path
 from flask import typing as flask_typing
+
+from lesson_02.job2.dal.convert_files import convert_files
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def main() -> flask_typing.ResponseReturnValue:
+    storage_path = Path(__file__).parents[3]/"storage"
     input_data: dict = request.json
     for key in ['stg_dir', 'raw_dir']:
         try:
@@ -13,9 +19,10 @@ def main() -> flask_typing.ResponseReturnValue:
             return {
                 "message": f"{key} parameter missed",
             }, 400
-    stg_dir = Path(__file__).parents[3] / "storage"/ f"{input_data.get('stg_dir')}"
-    raw_dir = Path(__file__).parents[3] / "storage"/ f"{input_data.get('raw_dir')}"
-
+    print(os.listdir(storage_path))
+    stg_dir = storage_path/ f"{input_data.get('stg_dir')}"
+    raw_dir = storage_path/ f"{input_data.get('raw_dir')}"
+    convert_files(stg_dir=stg_dir, raw_dir=raw_dir)
     return {
                "message": f"Data retrieved successfully",
            }, 201
